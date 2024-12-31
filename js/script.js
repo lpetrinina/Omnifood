07794a667e6013077355991fd5c93db5522da644
Luiza Petrinina <petrinina95@gmail.com>
2024-12-31 12:01:01 +0200

    Implemented Smooth Scrolling

--- js/script.js
+++ js/script.js
@@ -5,9 +5,42 @@ const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 // Make mobile navigation work
-const btnNav = document.querySelector(".btn-mobile-nav");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
-btnNav.addEventListener("click", () => {
+btnNavEl.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
+});
+
+// Smooth scrolling animation in case scroll-behavior does not work
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
 });
